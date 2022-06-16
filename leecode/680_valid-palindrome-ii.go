1dabff7c877d0c4bc4d087fc9ab1e580df88c3b7
王显朋 <wangxianpeng@youshu.cc>
2022-06-16 23:31:59 +0800

    leetcode 1991

--- leecode/680_valid-palindrome-ii.go
+++ leecode/680_valid-palindrome-ii.go
@@ -9,27 +9,33 @@ func ValidPalindrome(s string) bool {
 	var chars = []rune(s)
 	var left = 0
 	var right = len(chars) - 1
-	var flag = 0
 
-	for left <= right {
-		if flag > 1 {
-			return false
-		}
+	var leftFlag = true
+	var rightFlag = true
 
-		if chars[left] != chars[right] && chars[left+1] != chars[right] && chars[left] != chars[right-1] {
-			return false
-		}
-
+	for left < right {
 		if chars[left] == chars[right] {
 			left++
 			right--
-		} else if chars[left+1] == chars[right] {
-			flag++
-			left++
-		} else if chars[left] == chars[right-1] {
-			flag++
-			right--
+		} else {
+			// 多余的那个字符在右侧，跳过他继续检查是不是回文
+			for i, j := left, right-1; i < j; i, j = i+1, j-1 {
+				if chars[i] != chars[j] {
+					rightFlag = false
+					break
+				}
+			}
+			// 多余的那个字符在左侧， 跳过他继续检查是不是回文
+			for i, j := left+1, right; i < j; i, j = i+1, j-1 {
+				if chars[i] != chars[j] {
+					leftFlag = false
+					break
+				}
+			}
+			// 两种情况，只要有一种还是回文返回：是回文
+			return leftFlag || rightFlag
 		}
 	}
+
 	return true
 }
